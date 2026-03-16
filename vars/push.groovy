937df8cd6a012e41702fe3c286ee30c59a152e71
SunChip <fear.kaz@gmail.com>
2026-03-16 20:16:17 +0300

    fix push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,9 +1,11 @@
 def pushStage() {
     stage('Push stage') {
-        dir(serviceDir) {
-            echo "Pushing Docker image to registry..."
-            sh 'docker tag mytodo-service:${imageTag} myregistry/mytodo-service:${imageTag}'
-            sh 'docker push myregistry/mytodo-service:${imageTag}'
+        withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
+            dir(serviceDir) {
+                echo "Pushing Docker image to registry..."
+                sh "docker tag mytodo-service:${imageTag} myregistry/mytodo-service:${imageTag}"
+                sh "docker push myregistry/mytodo-service:${imageTag}"
+            }
         }
         echo "Pushing Docker image to registry complete"
     }
