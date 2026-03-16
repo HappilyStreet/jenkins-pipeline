185d5cda529e4085718947c2f8816c91cee4909d
SunChip <fear.kaz@gmail.com>
2026-03-16 19:32:53 +0300

    test

--- vars/build.groovy
+++ vars/build.groovy
@@ -13,6 +13,18 @@ def buildStage() {
             sh 'git checkout main'
             sh 'git pull origin main'
         }
+        withCredentials[usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]{
+            echo "Pull docket image"
+            
+            dir(serviceDir) {
+                echo "Logging in to Docker Registry..."
+                sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin'
+
+                echo "Building Docker image with tag: mytodo-service:${imageTag}"
+                sh "docker build -t mytodo-service:${imageTag} ."
+            }
+        }
+        echo "✅ Build Stage completed."
     }
 }
 return this
