6611d2792c4e5c23db22ec16131355a0bd829b8b
SunChip <fear.kaz@gmail.com>
2026-03-16 19:24:02 +0300

    test

--- vars/build.groovy
+++
@@ -1,5 +0,0 @@
-def call(String serviceDir, String imageTag) {
-    echo "Building service in ${serviceDir} with image tag ${imageTag}"
-
-    echo "Docker image myrepo/${imageTag} built successfully"
-}
