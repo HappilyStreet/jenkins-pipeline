f7fd64b3c58025b384e8c9fe6099287081f5a738
SunChip <fear.kaz@gmail.com>
2026-03-16 20:19:31 +0300

    fix push stage

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,12 +1,15 @@
 def pushStage() {
     stage('Push stage') {
-        withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
-            dir(serviceDir) {
-                echo "Pushing Docker image to registry..."
-                sh "docker tag mytodo-service:${imageTag} myregistry/mytodo-service:${imageTag}"
-                sh "docker push myregistry/mytodo-service:${imageTag}"
+         withEnv(["PATH=/usr/local/bin:$PATH"]) {
+            withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
+                dir(serviceDir) {
+                    echo "Pushing Docker image to registry..."
+                    sh "docker tag mytodo-service:${imageTag} myregistry/mytodo-service:${imageTag}"
+                    sh "docker push myregistry/mytodo-service:${imageTag}"
+                }
             }
-        }
+         }
+
         echo "Pushing Docker image to registry complete"
     }
 }
