38cf6232fd2c2b8617807f07e37923a05796868f
SunChip <fear.kaz@gmail.com>
2026-03-16 20:02:39 +0300

    edit build

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,26 +2,28 @@ def buildStage() {
     stage('Build Stage'){
         echo "🔹 Starting Build Stage"
 
-        echo "Clonee repo"
+        echo "Clone repo"
         dir(serviceDir) {
             sh 'git clone https://github.com/HappilyStreet/MyToDoService.git .'
         }
 
-        echo "Installin dependensies"
+        echo "Installing dependensies"
         dir(serviceDir) {
             sh 'git fetch --all'
             sh 'git checkout main'
             sh 'git pull origin main'
         }
-        withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
-            echo "Pull docket image"
-            
-            dir(serviceDir) {
-                echo "Logging in to Docker Registry..."
-                sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin'
+        withEnv(["PATH=/usr/local/bin:$PATH"]) {
+            withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
+                echo "Pull docket image"
+                
+                dir(serviceDir) {
+                    echo "Logging in to Docker Registry..."
+                    sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin'
 
-                echo "Building Docker image with tag: mytodo-service:${imageTag}"
-                sh "docker build -t mytodo-service:${imageTag} ."
+                    echo "Building Docker image with tag: mytodo-service:${imageTag}"
+                    sh "docker build -t mytodo-service:${imageTag} ."
+                }
             }
         }
         echo "✅ Build Stage completed."
