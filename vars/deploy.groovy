468bd58c21c9c740a232d7c719b2846ad62997c2
SunChip <fear.kaz@gmail.com>
2026-03-16 19:47:48 +0300

    fix

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -5,7 +5,7 @@ def call(serviceDir, imageTag){
         echo "KUBECONFIG path is: ${env.KUBECONFIG}"
         sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
 
-        dir(serviceDir){
+        dir("${serviceDir}/MyToDoService"){
             echo "Deploying to Kubernetes using Helm..."
             sh 'helm upgrade --install mytodo ./helm-chart --set image.tag=${imageTag}'
             }
