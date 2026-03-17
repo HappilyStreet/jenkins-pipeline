dc5100c79be9e0f583af65242fab5f606fa4740a
SunChip <fear.kaz@gmail.com>
2026-03-17 11:23:06 +0300

    edit deploy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,13 +1,14 @@
-def deployStage(){
+def deployStage(serviceDir, imageTag){
     stage('Deploy via helm'){
         withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]){
-            
-        echo "KUBECONFIG path is: ${env.KUBECONFIG}"
-        sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
+            withEnv(["PATH=/usr/local/bin:/opt/homebrew/bin:$PATH"]) {
+            echo "KUBECONFIG path is: ${env.KUBECONFIG}"
+            sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
 
-        dir("${serviceDir}/MyToDoService"){
-            echo "Deploying to Kubernetes using Helm..."
-            sh 'helm upgrade --install mytodo ./helm-chart --set image.tag=${imageTag}'
+            dir("${serviceDir}/MyToDoService"){
+                echo "Deploying to Kubernetes using Helm..."
+                sh 'helm upgrade --install mytodo ./helm-chart --set image.tag=${imageTag}'
+                }
             }
         }
         echo "✅ Deploy Stage completed."
