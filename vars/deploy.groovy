5007602f2fcd50b6038b7ec54e625dc5758d38d0
SunChip <fear.kaz@gmail.com>
2026-03-17 13:08:24 +0300

    test

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,16 +1,15 @@
 def deployStage(){
-    def serviceDir = "${env.WORKSPACE}/app"
     stage('Deploy via helm'){
         withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]){
             withEnv(["PATH=/usr/local/bin:/opt/homebrew/bin:$PATH"]) {
             echo "KUBECONFIG path is: ${env.KUBECONFIG}"
             sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
 
-            dir(serviceDir){
+            dir(${env.WORKSPACE}){
                 sh "pwd"
                 sh "ls -l ./helm"
                 echo "Deploying to Kubernetes using Helm..."
-                sh "helm upgrade --install mytodo ./helm --set image.tag=${imageTag}"
+                sh "helm upgrade --install mytodo .app/helm --set image.tag=${imageTag}"
                 }
             }
         }
