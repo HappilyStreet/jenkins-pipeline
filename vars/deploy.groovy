5daf6a00cda47c7c3d6ccd7092b0f575668d1a06
SunChip <fear.kaz@gmail.com>
2026-03-17 11:49:56 +0300

    fix path problem

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -5,7 +5,7 @@ def deployStage(){
             echo "KUBECONFIG path is: ${env.KUBECONFIG}"
             sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
 
-            dir("${serviceDir}/MyToDoService"){
+            dir("MyToDoService"){
                 echo "Deploying to Kubernetes using Helm..."
                 sh 'helm upgrade --install mytodo ./helm-chart --set image.tag=${imageTag}'
                 }
