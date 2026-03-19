a7417b7b36c2d04b5a99a70cef3d88726b1f73f8
SunChip <fear.kaz@gmail.com>
2026-03-19 14:16:52 +0300

    fix check

--- vars/check.groovy
+++ vars/check.groovy
@@ -5,16 +5,18 @@ def testStage() {
                 def values = readYaml file: 'values.yaml'
                 def servicePort = values.servicePort
                 println "servicePort = ${servicePort}"
-            }
-            echo "Порт: ${servicePort} используется прикладом"
-            withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
-                echo "RUN TEST"
-                def response = sh (
-                    script: "curl http:82.117.87.172:${servicePort}/health",
-                    returnStdout: true
-                ).trim()
-                if(!response.contains("ok")) {
-                    error("service down")
+
+                echo "Порт: ${servicePort} используется прикладом"
+                
+                withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
+                    echo "RUN TEST"
+                    def response = sh (
+                        script: "curl http:82.117.87.172:${servicePort}/health",
+                        returnStdout: true
+                    ).trim()
+                    if(!response.contains("ok")) {
+                        error("service down")
+                    }
                 }
             }
         echo "✅ Тесты выполнены"
