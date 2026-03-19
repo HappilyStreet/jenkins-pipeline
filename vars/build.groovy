1a1902e9cf88ac654f83f6675bac503ca17fa573
SunChip <fear.kaz@gmail.com>
2026-03-19 12:58:07 +0300

    add repo and jq install check

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,25 +1,29 @@
 def buildStage() {
-    stage('Build Stage'){
+    stage("Build Stage"){
         echo "🔹 Starting Build Stage"
-
         echo "Clone repo"
         dir(serviceDir) {
-            sh 'git clone https://github.com/HappilyStreet/MyToDoService.git .'
+            def file = new File("requirements.txt")
+            if(file.exists()) {
+                sh "git pull https://github.com/HappilyStreet/MyToDoService.git"
+            }
+            else {
+                sh "git clone https://github.com/HappilyStreet/MyToDoService.git ."
+            }
         }
-
         echo "Installing dependensies"
         dir(serviceDir) {
-            sh 'git fetch --all'
-            sh 'git checkout main'
-            sh 'git pull origin main'
+            sh "git fetch --all"
+            sh "git checkout main"
+            sh "git pull origin main"
         }
         withEnv(["PATH=/usr/local/bin:$PATH"]) {
-            withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
+            withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]){
                 echo "Pull docket image"
                 
                 dir(serviceDir) {
                     echo "Logging in to Docker Registry..."
-                    sh 'echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin'
+                    sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
 
                     echo "Building Docker image with tag: mytodo-service:${imageTag}"
                     sh "docker build -t mytodo-service:${imageTag} ."
--- vars/check.groovy
+++ vars/check.groovy
@@ -1,22 +1,33 @@
 def testStage() {
     stage('Run some tests into kuber') {
         withEnv(["PATH=/usr/local/bin:/opt/homebrew/bin:$PATH"]) {
-            def serviceport = sh (
-                script: "helm get values mytodo -n default -o json | jq -r '.service.port'"
+            def checkJQ = sh (
+                script: "which jq"
                 returnStdout: true
-            ).trim
-            echo "Порт: ${serviceport} используется прикладом"
+            ).trim()
+            if(!checkJQ.contains("jq")){
+                script: "apt update && apt install -y jq"
+            }
+            else {
+                echo "✅ jq установлен можно выполнять проверки"
+            }
+            def servicePort = sh (
+                script: "helm get values mytodo -n default -o json | jq -r '.service.port'",
+                returnStdout: true
+            ).trim()
+            echo "Порт: ${servicePort} используется прикладом"
         }
         withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
             echo "RUN TEST"
             def response = sh (
-            script: "curl http:82.117.87.172:${servicePort}/health"
+            script: "curl http:82.117.87.172:${servicePort}/health",
             returnStdout: true
-            ).trim
+            ).trim()
             if(!response.contains("ok")) {
                 error("service down")
             }
         }
+        echo "✅ Тесты выполнены"
     }
 }
 return this
