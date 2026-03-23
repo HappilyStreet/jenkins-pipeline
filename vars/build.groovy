e1b8c7cc77c8cb1bcd168d52eda0b5b17626605f
SunChip <fear.kaz@gmail.com>
2026-03-23 10:17:31 +0300

    fix

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,37 +2,36 @@ def buildStage() {
     stage("Checkout repository and install dependensies"){
         echo "🔹 Starting Build Stage"
         echo "Cloning repo"
-        
-        withEnv(["PATH=/usr/local/bin:$PATH"]) {
-            dir(serviceDir) {
-                if(fileExists(".git")) {
-                    echo "✅ Repo exists, pulling latest changes"
-                    sh "git reset --hard && git clean -fd"
-                    sh "git pull origin main"
-                }
-                else {
-                    echo "🔹Repo didnt exist and will be clone"
-                    sh "git clone https://github.com/HappilyStreet/MyToDoService.git ."
-                }
 
-                sh '''
-                    python3 -m venv .venv
-                    source .venv/bin/activate
-                    pip install --no-cache-dir flake8
-                    python -m flake8 .
-                '''
+        withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]) {
+            withEnv(["PATH=/usr/local/bin:$PATH"]) {
+                dir(serviceDir) {
+                    if(fileExists(".git")) {
+                        echo "✅ Repo exists, pulling latest changes"
+                        sh "git reset --hard && git clean -fd"
+                        sh "git pull origin main"
+                    }
+                    else {
+                        echo "🔹Repo didnt exist and will be clone"
+                        sh "git clone https://github.com/HappilyStreet/MyToDoService.git ."
+                    }
+
+                docker.image('python:3.11-slim').inside {
+                    sh '''
+                        pip install --no-cache-dir flake8
+                        flake8 .
+                    '''
+                }
 
                 echo "✅ Checkout complete"
                 echo "✅ linter check complete"     
-            }
-            withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]) {
+
                 echo "Logging in to Docker Registry..."
                 sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
 
                 echo "Building Docker image with tag: mytodo-service:${imageTag}"
                 sh "docker build -t mytodo-service:${imageTag} ${serviceDir}"
             }
-
         }
     }
     echo "✅  Image was builded"
