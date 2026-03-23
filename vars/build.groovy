433735a14aaa95be5bf0004d20026f7960c3ac92
SunChip <fear.kaz@gmail.com>
2026-03-23 10:09:06 +0300

    refactor

--- vars/build.groovy
+++ vars/build.groovy
@@ -4,36 +4,31 @@ def buildStage() {
         echo "Cloning repo"
         
         withEnv(["PATH=/usr/local/bin:$PATH"]) {
+            dir(serviceDir) {
+                if(fileExists(".git")) {
+                    echo "✅ Repo exists, pulling latest changes"
+                    sh "git reset --hard && git clean -fd"
+                    sh "git pull origin main"
+                }
+                else {
+                    echo "🔹Repo didnt exist and will be clone"
+                    sh "git clone https://github.com/HappilyStreet/MyToDoService.git ."
+                }
+                
+                sh '''
+                    pip install --no-cache-dir flake8
+                    flake8 .
+                '''
+
+                echo "✅ Checkout complete"
+                echo "✅ linter check complete"     
+            }
             withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]) {
-                dir(serviceDir) {
-                    if(fileExists(".git")) {
-                        echo "✅ Repo exists, pulling latest changes"
-                        sh "git reset --hard && git clean -fd"
-                        sh "git pull origin main"
-                    }
-                    else {
-                        echo "🔹Repo didnt exist and will be clone"
-                        sh "git clone https://github.com/HappilyStreet/MyToDoService.git ."
-                    }
+                echo "Logging in to Docker Registry..."
+                sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
 
-                    docker.image('python:3.11-slim').inside {
-                        sh '''
-                            pip install --no-cache-dir flake8
-                            flake8 .
-                        '''
-                    }
-                    echo "✅ Checkout complete"
-                    echo "✅ linter check complete"  
-
-                    echo "Pull docket image"
-                    echo "Logging in to Docker Registry..."
-
-                    sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
-
-                    echo "Building Docker image with tag: mytodo-service:${imageTag}"
-                    sh "docker build -t mytodo-service:${imageTag} ."
-
-                }      
+                echo "Building Docker image with tag: mytodo-service:${imageTag}"
+                sh "docker build -t mytodo-service:${imageTag} ${serviceDir}"
             }
 
         }
