55644faba9b84c38e5e2d2d094a0c7211b9c4fce
SunChip <fear.kaz@gmail.com>
2026-03-23 09:52:19 +0300

    refactor

--- vars/build.groovy
+++ vars/build.groovy
@@ -2,50 +2,39 @@ def buildStage() {
     stage("Checkout repository and install dependensies"){
         echo "🔹 Starting Build Stage"
         echo "Cloning repo"
+        
         withEnv(["PATH=/usr/local/bin:$PATH"]) {
-            docker.image('python:3.11-slim').inside {
-                sh '''
-                    pip install flake8
-                    flake8 .
-                '''
-            }
-            
+
             dir(serviceDir) {
                 if(fileExists(".git")) {
-                    echo "✅ Repo exist"
-                    sh "git pull https://github.com/HappilyStreet/MyToDoService.git"
+                    echo "✅ Repo exists, pulling latest changes"
+                    sh "git reset --hard && git clean -fd"
+                    sh "git pull origin main"
                 }
                 else {
                     echo "🔹Repo didnt exist and will be clone"
                     sh "git clone https://github.com/HappilyStreet/MyToDoService.git ."
                 }
 
-                sh "git fetch --all"
-                sh "git checkout main"
-                sh "git pull origin main"
-
+                docker.image('python:3.11-slim').inside {
+                    sh '''
+                        pip install --no-cache-dir flake8
+                        flake8 .
+                    '''
+                }
                 
             }
             echo "✅ Checkout complete"
-
-            dir(serviceDir) {
-                sh '''
-                    pip install --no-cache-dir flake8
-                    flake8 .
-                '''
-            }
             echo "✅ linter check complete"  
 
             withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]){
                 echo "Pull docket image"
+                echo "Logging in to Docker Registry..."
                 
-                dir(serviceDir) {
-                    echo "Logging in to Docker Registry..."
-                    sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
+                sh "echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin"
 
-                    echo "Building Docker image with tag: mytodo-service:${imageTag}"
-                    sh "docker build -t mytodo-service:${imageTag} ."
-                }
+                echo "Building Docker image with tag: mytodo-service:${imageTag}"
+                sh "docker build -t mytodo-service:${imageTag} ."
             }   
         }
     }
