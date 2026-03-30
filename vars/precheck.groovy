7c48af4f97ee59ecd5537d41ad9c967c695fbba9
SunChip <fear.kaz@gmail.com>
2026-03-30 13:09:20 +0300

    fix precheck

--- vars/precheck.groovy
+++ vars/precheck.groovy
@@ -28,7 +28,14 @@ def precheckStage() {
             ).trim()
             if(!checkkubectl.contains("/usr/bin/kubectl")){
                 echo "Kubectl isnt install, installing"
-                sh "mkdir -p \$HOME/bin && curl -LO https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl \$HOME/bin/ && export PATH=\$HOME/bin:\$PATH && kubectl version --client"
+                sh """
+                    mkdir -p \$HOME/bin
+                    curl -LO https://dl.k8s.io/release/\$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+                    chmod +x kubectl
+                    mv kubectl \$HOME/bin/
+                    export PATH=\$HOME/bin:\$PATH
+                    kubectl version --client
+                """
             }
         }
 
