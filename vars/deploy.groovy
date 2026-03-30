d40eea484123aaa38d234433c9b81adebc94e813
SunChip <fear.kaz@gmail.com>
2026-03-30 13:18:48 +0300

    fix

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,7 +2,14 @@ def deployStage(){
     stage('Deploy via helm'){
         withEnv(["PATH=${env.HOME}/bin:${env.PATH}"]) {
             withCredentials([string(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]){
+            sh '''
+                    echo "$KUBECONFIG_SECRET" > $HOME/kubeconfig_temp
+                    export KUBECONFIG=$HOME/kubeconfig_temp
+                    kubectl get nodes
+            '''
+
             echo "KUBECONFIG path is: ${env.KUBECONFIG}"
+
             sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
 
             dir(serviceDir){
@@ -11,6 +18,9 @@ def deployStage(){
                 echo "Deploying to Kubernetes using Helm..."
                 sh "helm upgrade --install mytodo ./helm --set image.tag=${imageTag}"
                 }
+
+            sh 'rm -f $HOME/kubeconfig_temp'
+            
             }
         }
         echo "✅ Deploy Stage completed."
