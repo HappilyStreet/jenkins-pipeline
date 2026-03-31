a9a4237e5368713444afea55b76c1952abcaef39
SunChip <fear.kaz@gmail.com>
2026-03-31 14:29:35 +0300

    fix test stage

--- vars/tests.groovy
+++ vars/tests.groovy
@@ -1,16 +1,17 @@
 def testStage() {
-    echo "🔹 Running tests фаеук вуздщн"
+    echo "🔹 Running tests after deploy"
+
     dir("${serviceDir}/helm") {
         sh '''
-        . venv/bin/activate
-        pytest tests/test_service.py --alluredir=allure-results
+            python3 -m venv venv
+            venv/bin/pip install -r requirements.txt
+            venv/bin/pytest tests/test_service.py --alluredir=allure-results
         '''
 
         allure([
             includeProperties: false,
             jdk: '',
-            results: [[path: "${serviceDir}/allure-results"]]
+            results: [[path: "${serviceDir}/helm/allure-results"]]
         ])
     }
 }
-return this
