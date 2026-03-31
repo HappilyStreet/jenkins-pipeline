cd1e60c7afab154c19a47ff4d4680614a3c678fe
SunChip <fear.kaz@gmail.com>
2026-03-31 13:47:07 +0300

    fix

--- vars/packages.groovy
+++ vars/packages.groovy
@@ -19,33 +19,30 @@ def packagesStage() {
 
 
             sh '''
-                # Установим pip, если нет
-                if ! command -v pip3 >/dev/null 2>&1; then
-                    echo "Installing pip..."
-                    curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-                    python3 get-pip.py --user
-                fi
+            # Установим pip, если нет
+            if ! command -v pip3 >/dev/null 2>&1; then
+                echo "Installing pip..."
+                curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+                python3 get-pip.py --user
+            fi
 
-                # Создаём виртуальное окружение
-                python3 -m venv venv
-                . venv/bin/activate
+            # Создаём виртуальное окружение
+            python3 -m venv venv
+            . venv/bin/activate
 
-                # Обновляем pip
-                pip install --upgrade pip
+            # Обновляем pip
+            pip install --upgrade pip
 
-                # Устанавливаем зависимости из requirements.txt
-                if [ -f requirements.txt ]; then
-                    pip install -r requirements.txt
-                else
-                    # Если requirements.txt нет, ставим стандартный набор
-                    pip install pytest pytest-cov allure-pytest pylint flask sqlalchemy requests
-                fi
+            # Устанавливаем зависимости
+            if [ -f requirements.txt ]; then
+                pip install -r requirements.txt
+            else
+                pip install pytest pytest-cov allure-pytest pylint flask sqlalchemy requests
+            fi
 
-                # Проверка кода
-                pylint **/*.py || true
-
-                # Запуск тестов
-                pytest --alluredir=allure-results
+            # Проверка кода и тесты уже в активированном venv
+            venv/bin/pylint **/*.py || true
+            venv/bin/pytest --alluredir=allure-results
             '''
 
             allure([
