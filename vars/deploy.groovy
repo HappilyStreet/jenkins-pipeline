260b67b735f0fd59086a28587541d3d70eabe554
SunChip <fear.kaz@gmail.com>
2026-03-31 11:02:43 +0300

    edit deloy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,14 +1,28 @@
 def deployStage(){
-    withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]){
+    withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
         withEnv(["PATH=${env.HOME}/bin:${env.PATH}"]) {
-        echo "KUBECONFIG path is: ${env.KUBECONFIG}"
-        sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
+            if(params.BUILD == 'install') {
+                echo "🔹 BUILD=install → fetching latest image tag from Docker Hub"
+                def repo = 'mrsunchip/mytodo-service'
 
-        dir(serviceDir){
-            sh "pwd"
-            sh "ls -l ./helm"
-            echo "Deploying to Kubernetes using Helm..."
-            sh "helm upgrade --install mytodo ./helm --set image.tag=${imageTag}"
+                def response = sh (
+                    script: "curl -s https://hub.docker.com/v2/repositories/${repo}/tags?page_size=1&page=1&ordering=last_updated",
+                    returnStdout: true
+                ).trim()
+            
+                def json = readJSON text: response
+                imageTag = json.results[0].name
+
+                echo "✅ Latest image tag from Docker Hub: ${imageTag}"
+
+                echo "KUBECONFIG path is: ${env.KUBECONFIG}"
+                sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
+                dir(serviceDir){
+                    sh "pwd"
+                    sh "ls -l ./helm"
+                    echo "Deploying to Kubernetes using Helm..."
+                    sh "helm upgrade --install mytodo ./helm --set image.tag=${imageTag}"
+                }
             }
         }
     }
