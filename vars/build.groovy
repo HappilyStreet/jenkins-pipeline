fe6de21bbf2400ac22a1c44dc963a5fdc14db535
SunChip <fear.kaz@gmail.com>
2026-03-31 11:53:13 +0300

    fix stages

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,13 +1,20 @@
 def buildStage() {
     dir(serviceDir) {
-        withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]) {
-        echo "Logging in to Docker Registry..."
-        sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
+        withEnv(["PATH=/usr/local/bin:$PATH"]) {
+            withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]) {
+                echo "Logging in to Docker Registry..."
+                sh "echo \$DOCKER_PASS | docker login -u \$DOCKER_USER --password-stdin"
 
-        echo "Building Docker image with tag: mytodo-service:${imageTag}"
-        sh "docker build -t mytodo-service:${imageTag} ${serviceDir}"     
+                echo "Building Docker image with tag: mytodo-service:${imageTag}"
+                sh "docker build -t mytodo-service:${imageTag} ${serviceDir}"   
 
+                echo "Pushing Docker image to registry..."
+                sh "docker tag mytodo-service:${imageTag} mrsunchip/mytodo-service:${imageTag}"
+                sh "docker push mrsunchip/mytodo-service:${imageTag}"  
+
+            }
         }
+
     }
     echo "✅  Builded and pushed to docker hub"
 }
--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -2,26 +2,28 @@ def deployStage(){
     withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG')]) {
         withEnv(["PATH=${env.HOME}/bin:${env.PATH}"]) {
             if(params.BUILD == 'install') {
-                echo "🔹 BUILD=install → fetching latest image tag from Docker Hub"
-                def repo = 'mrsunchip/mytodo-service'
+                withCredentials([usernamePassword(credentialsId: "dockerhub-creds", usernameVariable: "DOCKER_USER", passwordVariable: "DOCKER_PASS")]) {
+                    echo "🔹 BUILD=install → fetching latest image tag from Docker Hub"
+                    def repo = 'mrsunchip/mytodo-service'
 
-                def response = sh (
-                    script: "curl -s https://hub.docker.com/v2/repositories/${repo}/tags?page_size=1&page=1&ordering=last_updated",
-                    returnStdout: true
-                ).trim()
-            
-                def json = readJSON (text: response)
-                imageTag = json.results[0].name
+                    def response = sh (
+                        script: "curl -s https://hub.docker.com/v2/repositories/${repo}/tags?page_size=1&page=1&ordering=last_updated",
+                        returnStdout: true
+                    ).trim()
+                
+                    def json = readJSON (text: response)
+                    imageTag = json.results[0].name
 
-                echo "✅ Latest image tag from Docker Hub: ${imageTag}"
+                    echo "✅ Latest image tag from Docker Hub: ${imageTag}"
 
-                echo "KUBECONFIG path is: ${env.KUBECONFIG}"
-                sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
-                dir(serviceDir){
-                    sh "pwd"
-                    sh "ls -l ./helm"
-                    echo "Deploying to Kubernetes using Helm..."
-                    sh "helm upgrade --install mytodo ./helm --set image.tag=${imageTag}"
+                    echo "KUBECONFIG path is: ${env.KUBECONFIG}"
+                    sh "kubectl get nodes --kubeconfig ${env.KUBECONFIG}"
+                    dir(serviceDir){
+                        sh "pwd"
+                        sh "ls -l ./helm"
+                        echo "Deploying to Kubernetes using Helm..."
+                        sh "helm upgrade --install mytodo ./helm --set image.tag=${imageTag}"
+                    }
                 }
             }
         }
