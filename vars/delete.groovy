b2b566eb1926977ee998248bc429f9683f7ae752
SunChip <fear.kaz@gmail.com>
2026-04-13 16:14:35 +0300

    fix

--- vars/delete.groovy
+++ vars/delete.groovy
@@ -1,13 +1,14 @@
 def deleteStage() {
     stage('Delete service') {
+        // def kubeconfig = env.KUBE_CONFIG_PATH
         withEnv(["PATH=${env.HOME}/bin:${env.PATH}"]){
             dir(serviceDir){
                 
                 echo "Uninstall using Helm..."
-                sh "helm uninstall mytodo -n default --kubeconfig {$env.KUBE_CONFIG_PATH}"
+                sh "helm uninstall mytodo -n default --kubeconfig ${env.KUBE_CONFIG_PATH}"
             }
             def deleteResult = sh(
-                script: "kubectl get deploy --kubeconfig {$env.KUBE_CONFIG_PATH}",
+                script: "kubectl get deploy --kubeconfig ${env.KUBE_CONFIG_PATH}",
                 returnStdout: true
             ).trim()
             if(deleteResult.contains("mytodoservice-deployment")){
